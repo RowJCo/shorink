0f6df145dde3bbec4897b92340ace13f97185250
RowJCo <163330547+RowJCo@users.noreply.github.com>
2024-08-05 21:58:18 +0100

    Add basic folder structure

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/components/*.{js,jsx}", "./src/App.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
