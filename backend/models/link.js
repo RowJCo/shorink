e0d3abaf914c02ce475e935b9079e3e3f6fe8148
RowJCo <163330547+RowJCo@users.noreply.github.com>
2024-08-05 23:30:34 +0100

    Restructure link model to better match the controller

--- backend/models/link.js
+++ backend/models/link.js
@@ -1,14 +1,27 @@
 const mongoose = require('mongoose');
 
 const linkSchema = new mongoose.Schema({
-    longLink: {
+    link: {
         type: String,
         required: true
     },
-    shortLink: {
+    key: {
         type: String,
-        required: true
+        unique: true,
+        default: generateRandomKey = () => {
+            //Creates a random 5 character key
+            const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            let key = '';
+            for (let i = 0; i < 5; i++) {
+                key += characters.charAt(Math.floor(Math.random() * characters.length));
+            }
+            return key;
+        }
     },
+    clicks: {
+        type: Number,
+        default: 0
+    }
 });
 
 const link = mongoose.model('link', linkSchema);
