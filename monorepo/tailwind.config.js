4ad6be4b5b271749f4906492f2bacb621ddcf0bc
RowJCo <163330547+RowJCo@users.noreply.github.com>
2024-08-13 00:17:29 +0100

    Remove uneeded frontend and backend folders

--- monorepo/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/components/*.{js,jsx}", "./src/App.jsx", "./src/pages/*.{js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
