dbe757b2cdfd38b76636a727d987305540bf1ab2
RowJCo <163330547+RowJCo@users.noreply.github.com>
2024-08-13 14:17:45 +0100

    Update build path in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/components/*.{js,jsx}", "./src/App.jsx", "./src/pages/*.{js,jsx}"],
+  content: [".build/index.html", "./src/components/*.{js,jsx}", "./src/App.jsx", "./src/pages/*.{js,jsx}"],
   theme: {
     extend: {},
   },
